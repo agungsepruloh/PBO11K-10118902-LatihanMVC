199934af98471afab20cfbaf44abfa67fd68f6fb
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-14 05:17:45 +0700

    Java Application using MVC Pattern

---
+++ src/com/pbo/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pbo.latihanmvc.event;
+
+import com.pbo.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author : AGUNGSEPRULOH
+ * NIM     : 10118902
+ * Kelas   : IF-11K
+ * 
+ */
+public interface PelangganListener {
+    public void onChange(PelangganModel pelangganModel);
+}
